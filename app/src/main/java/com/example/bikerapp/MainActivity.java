318adbc8524723b935b617355e72af694282f0f3
Alberto Solaro <albeluca95@gmail.com>
2019-05-20 18:47:50 +0200

    Notification New Order to delivery

--- app/src/main/java/com/example/bikerapp/MainActivity.java
+++ app/src/main/java/com/example/bikerapp/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.bikerapp;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -23,10 +31,13 @@ import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.Collections;
+
+import static android.support.v4.app.NotificationCompat.VISIBILITY_PUBLIC;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,6 +47,10 @@ public class MainActivity extends AppCompatActivity {
     private static final String bikerDataFile = "BikerDataFile";
     public static ArrayList<ReservationModel> reservationsData;
     private ReservationListAdapter reservationsAdapter;
+    private String NOTIFICATION_CHANNEL_ID = "com.example.bikerapp";
+    private int unique_id = 1;
+    private NotificationCompat.Builder builder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +72,20 @@ public class MainActivity extends AppCompatActivity {
 
         //Get Firestore instance
         db = FirebaseFirestore.getInstance();
+
+        createNotificationChannel();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_restaurant)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.biker_logo))
+                .setContentTitle("New Order to Delivery")
+                .setContentIntent(pendingIntent)
+                .setVisibility(VISIBILITY_PUBLIC)
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
 
         reservationsData = new ArrayList<>();
         fillWithData();
@@ -104,7 +133,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void fillWithData() {
-
         db.collection("reservations").whereEqualTo("biker_id", bikerKey).addSnapshotListener((EventListener<QuerySnapshot>) (document, e) -> {
 
             if (e != null)
@@ -112,13 +140,8 @@ public class MainActivity extends AppCompatActivity {
             reservationsData.clear();
 
             for(DocumentChange dc : document.getDocumentChanges())
-                if(dc.getType() == DocumentChange.Type.ADDED)
-                {
-                    //TODO incremento
-                }
-            if (!document.isEmpty()) {
-                for (DocumentSnapshot doc : document) {
-
+                if(dc.getType() == DocumentChange.Type.ADDED){
+                    DocumentSnapshot doc = dc.getDocument();
                     ReservationModel tmpReservationModel = new ReservationModel((Long)doc.get("rs_id"),
                             (String) doc.get("rest_name"),
                             (String) doc.get("rest_address"),
@@ -129,23 +152,36 @@ public class MainActivity extends AppCompatActivity {
                             (String) doc.get("cust_id"),
                             (String) doc.get("cust_phone"),
                             (Timestamp) doc.get("delivery_time")
-
-
                     );
                     reservationsData.add(tmpReservationModel);
                     reservationsAdapter.notifyDataSetChanged();
+                    Collections.sort(reservationsData);
 
+                    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+                    // notificationId is a unique int for each notification that you must define
+                    notificationManager.notify(unique_id, builder.build());
+                    unique_id++;
                 }
-                Collections.sort(reservationsData);
-
-            } else {
-                Log.d("QueryReservation", "No such document");
-            }
-
         });
 
 
 
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String description = "Non so dove finisca questa descrizione";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "BikerApp", importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     @Override
